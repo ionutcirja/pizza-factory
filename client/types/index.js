f5472d2bac5b7c6fe82699d663f4d763d727652f
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-12 08:38:31 +0000

    create initial form field props global type

--- client/types/index.js
+++ client/types/index.js
@@ -1,3 +1,26 @@
 // @flow
 
+export type FormFieldProps = {
+  field: {
+    name: string,
+    value: any,
+    onChange: Function,
+    onBlur: Function,
+  },
+  form: {
+    touched: {
+      [key: string]: any,
+    },
+    errors: {
+      [key: string]: any,
+    },
+    values: {
+      [key: string]: any,
+    },
+    setFieldValue: Function,
+    setFieldError: Function,
+    setFieldTouched: Function,
+  },
+};
+
 export type State = {};
