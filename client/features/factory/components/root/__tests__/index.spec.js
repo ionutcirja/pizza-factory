e2cdb57c04b94dd3437e52e6725eae5e55708f6f
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-13 13:29:26 +0000

    update tests

--- client/features/factory/components/root/__tests__/index.spec.js
+++ client/features/factory/components/root/__tests__/index.spec.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Factory from '..';
+import Factory, { Sizes } from '..';
+
+describe('Sizes component', () => {
+  const propsToRender = {
+    options: [
+      'option 1',
+      'option 2',
+      'option 3',
+      'option 4',
+    ],
+  };
+  
+  describe('render', () => {
+    it('should render a select form field component', () => {
+      const wrapper = shallow(<Sizes {...propsToRender} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
 
 describe('Factory component', () => {
   describe('render', () => {
