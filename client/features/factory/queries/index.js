a9548d97e714a6dc93aa27b61c80d36d256953a4
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-13 10:24:19 +0000

    create gql queries / get pizza sizes and get pizza sizes by name

---
+++ client/features/factory/queries/index.js
@@ -0,0 +1,25 @@
+import gql from 'graphql-tag';
+
+export const GET_SIZES = gql`
+{
+  pizzaSizes {
+    name
+  }
+}
+`;
+
+export const GET_SIZE_BY_NAME = gql`
+  query pizzaSizeByName($name: PizzaSizes!) {
+    pizzaSizeByName(name: $name) {
+      maxToppings,
+      basePrice,
+      toppings {
+        topping {
+          name,
+          price,
+        },
+        defaultSelected,
+      }
+    }
+  }
+`;
