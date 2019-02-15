d91ca2e416547fb2ccc8f172fb45232651bd8c82
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-15 12:45:14 +0000

    plug in checkbox group field component / reset field value on unmount

--- client/features/forms/components/form-field/__tests__/index.spec.js
+++ client/features/forms/components/form-field/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import FormField from '..';
+import FormField, { FieldWrapper } from '..';
 
 jest.mock('../text', () => () => <input type="text" />);
 
@@ -32,6 +32,88 @@ describe('FormField component', () => {
       ];
       const wrapper = shallow(<FormField {...propsToRender} />);
       expect(wrapper).toMatchSnapshot();
+    });
+  
+    it('should render a CheckboxGroupField component if type prop value is checkbox_group', () => {
+      propsToRender.type = 'checkbox_group';
+      propsToRender.list = [
+        {
+          value: 1,
+        },
+        {
+          value: 2,
+        },
+        {
+          value: 3,
+        },
+      ];
+      const wrapper = shallow(<FormField {...propsToRender} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+  
+  describe('componentWillUnmount', () => {
+    it('should reset the field value to an empty string if isFieldArray prop value is falsy', () => {
+      propsToRender = {
+        type: 'text',
+        name: 'id',
+        field: {
+          name: 'id',
+        },
+        form: {
+          touched: {},
+          errors: {},
+          setFieldValue: jest.fn(),
+          setFieldError: jest.fn(),
+          setFieldTouched: jest.fn(),
+        },
+      };
+      const wrapper = shallow(<FieldWrapper {...propsToRender} />);
+      wrapper.instance().componentWillUnmount();
+      expect(propsToRender.form.setFieldValue).toHaveBeenCalledWith(
+        'id',
+        '',
+      );
+      expect(propsToRender.form.setFieldError).toHaveBeenCalledWith(
+        'id',
+        '',
+      );
+      expect(propsToRender.form.setFieldTouched).toHaveBeenCalledWith(
+        'id',
+        false,
+      );
+    });
+  
+    it('should reset the field value to an empty array if isFieldArray prop value is truthy', () => {
+      propsToRender = {
+        type: 'text',
+        name: 'id',
+        field: {
+          name: 'id',
+        },
+        isFieldArray: true,
+        form: {
+          touched: {},
+          errors: {},
+          setFieldValue: jest.fn(),
+          setFieldError: jest.fn(),
+          setFieldTouched: jest.fn(),
+        },
+      };
+      const wrapper = shallow(<FieldWrapper {...propsToRender} />);
+      wrapper.instance().componentWillUnmount();
+      expect(propsToRender.form.setFieldValue).toHaveBeenCalledWith(
+        'id',
+        [],
+      );
+      expect(propsToRender.form.setFieldError).toHaveBeenCalledWith(
+        'id',
+        '',
+      );
+      expect(propsToRender.form.setFieldTouched).toHaveBeenCalledWith(
+        'id',
+        false,
+      );
     });
   });
 });
--- client/features/forms/components/form-field/index.js
+++ client/features/forms/components/form-field/index.js
@@ -1,24 +1,54 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { Field } from 'formik';
 import TextField from './text';
 import SelectField from './select';
+import CheckboxGroupField from './checkbox-group';
+import type { FormFieldProps } from '../../../../types';
 
 const fieldComponentMap = {
   text: TextField,
   select: SelectField,
+  checkbox_group: CheckboxGroupField,
 };
 
 const getFieldComponent = (type: string = 'text') => fieldComponentMap[type];
 
+
+type WrapperProps = FormFieldProps & {
+  type: string,
+  isFieldArray: boolean,
+};
+
+export class FieldWrapper extends Component<WrapperProps> {
+  componentWillUnmount() {
+    const {
+      isFieldArray,
+      field,
+      form,
+    } = this.props;
+    
+    form.setFieldValue(field.name, isFieldArray ? [] : '');
+    form.setFieldError(field.name, '');
+    form.setFieldTouched(field.name, false);
+  }
+  
+  render() {
+    const { type, ...rest } = this.props;
+    const FieldComponent = getFieldComponent(type);
+    return <FieldComponent {...rest} />;
+  }
+}
+
 type Props = {
+  name: string,
   type?: string,
 };
 
-const FormField = ({ type, ...rest }: Props) => (
+const FormField = (props: Props) => (
   <Field
-    {...rest}
-    component={getFieldComponent(type)}
+    {...props}
+    component={FieldWrapper}
   />
 );
 
