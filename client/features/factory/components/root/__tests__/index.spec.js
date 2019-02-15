d56ed52481c19ad23900e07c0f7bc0edde549096
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-15 12:57:48 +0000

    plug in ingredients query / reset toppings value when size is changing

--- client/features/factory/components/root/__tests__/index.spec.js
+++ client/features/factory/components/root/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Factory, { Sizes } from '..';
+import Factory, { Sizes, Ingredients } from '..';
 
 describe('Sizes component', () => {
   const propsToRender = {
@@ -20,11 +20,69 @@ describe('Sizes component', () => {
   });
 });
 
+describe('Ingredients component', () => {
+  const propsToRender = {
+    maxToppings: 2,
+    toppings: [
+      {
+        defaultSelected: true,
+        topping: {
+          name: 'option 1',
+          price: 1,
+        },
+      },
+      {
+        defaultSelected: false,
+        topping: {
+          name: 'option 2',
+          price: 2,
+        },
+      },
+      {
+        defaultSelected: false,
+        topping: {
+          name: 'option 3',
+          price: 3,
+        },
+      },
+    ],
+  };
+  
+  describe('render', () => {
+    it('should render a checkbox group form field component', () => {
+      const wrapper = shallow(<Ingredients {...propsToRender} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
+
 describe('Factory component', () => {
+  const propsToRender = {
+    values: {
+      size: '',
+    },
+    setFieldValue: jest.fn(),
+  };
+  
   describe('render', () => {
     it('should render a sizes query component', () => {
-      const wrapper = shallow(<Factory />);
+      const wrapper = shallow(<Factory {...propsToRender} />);
       expect(wrapper).toMatchSnapshot();
+    });
+  
+    it('should render an ingredient query component if size value is truthy', () => {
+      propsToRender.values.size = 'large';
+      const wrapper = shallow(<Factory {...propsToRender} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+  
+  describe('componentWillUpdate', () => {
+    it('should reset toppings form value if sizes value is updated', () => {
+      propsToRender.values.size = 'small';
+      const wrapper = shallow(<Factory {...propsToRender} />);
+      expect(wrapper.instance().componentDidUpdate({ values: { size: 'large' } }));
+      expect(propsToRender.setFieldValue).toHaveBeenCalledWith('toppings', []);
     });
   });
 });
--- client/features/factory/components/root/index.js
+++ client/features/factory/components/root/index.js
@@ -1,6 +1,7 @@
 // @flow
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import SizesQuery from '../../containers/sizes-query';
+import IngredientsQuery from '../../containers/ingredients-query';
 import FormField from '../../../forms/components/form-field';
 import Loading from '../loading';
 import Error from '../error';
@@ -18,20 +19,76 @@ export const Sizes = ({ options }: SizesProps) => (
   />
 );
 
+type IngredientsProps = {
+  maxToppings: number | null,
+  toppings: Array<{
+    defaultSelected: boolean,
+    topping: {
+      name: string,
+      price: number,
+    }
+  }>
+};
+
+export const Ingredients = ({
+  maxToppings,
+  toppings,
+}: IngredientsProps) => (
+  <Fragment>
+    <FormField
+      name="toppings"
+      type="checkbox_group"
+      isFieldArray
+      list={toppings.map(item => (
+        {
+          label: `${item.topping.name} (${item.topping.price})`,
+          value: item.topping.price,
+        }
+      ))}
+      maxAllowed={maxToppings}
+    />
+  </Fragment>
+);
+
 type Props = {
+  values: {
+    size: string,
+  },
+  setFieldValue: Function,
   handleSubmit: Function,
 };
 
-const Factory = ({
-  handleSubmit,
-}: Props) => (
-  <form onSubmit={handleSubmit}>
-    <SizesQuery
-      LoadingWrapper={Loading}
-      ErrorWrapper={Error}
-      Component={Sizes}
-    />
-  </form>
-);
+class Factory extends Component<Props> {
+  componentDidUpdate(prevProps: Props) {
+    const { setFieldValue, values } = this.props;
+    if (values.size !== prevProps.values.size) {
+      setFieldValue('toppings', []);
+    }
+  }
+  
+  render() {
+    const { values, handleSubmit } = this.props;
+    
+    return (
+      <form onSubmit={handleSubmit}>
+        <SizesQuery
+          LoadingWrapper={Loading}
+          ErrorWrapper={Error}
+          Component={Sizes}
+        />
+        {values.size
+        && (
+          <IngredientsQuery
+            size={values.size.toUpperCase()}
+            LoadingWrapper={Loading}
+            ErrorWrapper={Error}
+            Component={Ingredients}
+          />
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 export default Factory;
