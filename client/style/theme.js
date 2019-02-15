27c1f21f271cc053d6810cc77a52920fa2104216
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-15 14:59:11 +0000

    create simple theme

---
+++ client/style/theme.js
@@ -0,0 +1,15 @@
+export default {
+  colours: {
+    white: '#ffffff',
+    black: '#111111',
+    blue: '#7eb0d5',
+    darkBlue: '#3f5a77',
+    turquoise: '#8bd3c7',
+    red: '#ef705f',
+  },
+  bezier: {
+    easeOutCubic: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    easeInCubic: 'cubic-bezier(0.55, 0.055, 0.675, 0.19)',
+    easeInOutCubic: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  },
+};
