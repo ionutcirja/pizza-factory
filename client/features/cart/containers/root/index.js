cd9091933c9d48cfb737625718874c6847569db4
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-18 13:14:31 +0000

    pass actions to root component

--- client/features/cart/containers/root/index.js
+++ client/features/cart/containers/root/index.js
@@ -1,13 +1,21 @@
 // @flow
 import { connect } from 'react-redux';
+import type { Dispatch } from 'redux';
+import { bindActionCreators } from 'redux';
 import { cartListIdsSelector } from '../../selectors';
 import type { State } from '../../../../types';
 import Component from '../../components/root';
+import * as Actions from '../../actions';
 
 const mapStateToProps = (state: State) => ({
   list: cartListIdsSelector(state),
 });
 
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  actions: bindActionCreators(Actions, dispatch),
+});
+
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(Component);
