1c13615efa8ca441b292aa341a093ef6de6400df
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-18 11:52:56 +0000

    create remove from cart and update quantity actions

--- client/features/cart/actions/index.js
+++ client/features/cart/actions/index.js
@@ -3,3 +3,9 @@ import { createAction } from 'redux-actions';
 
 export const ADD_TO_CART: string = 'ADD_TO_CART';
 export const addToCart = createAction(ADD_TO_CART);
+
+export const REMOVE_FROM_CART: string = 'REMOVE_FROM_CART';
+export const removeFromCart = createAction(REMOVE_FROM_CART);
+
+export const UPDATE_QUANTITY: string = 'UPDATE_QUANTITY';
+export const updateQuantity = createAction(UPDATE_QUANTITY);
