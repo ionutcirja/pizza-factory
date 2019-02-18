23f9bcb834d1f2ef77c3d1bcc45c9a13b19f742c
Ionut Cirja <ionut.cirja@gmail.com>
2019-02-18 11:57:19 +0000

    create list ids, id and totals selectors

--- client/features/cart/selectors/__tests__/index.spec.js
+++ client/features/cart/selectors/__tests__/index.spec.js
@@ -1,6 +1,9 @@
 import {
   cartListSelector,
   cartListNumSelector,
+  cartListIdsSelector,
+  cartIdSelector,
+  cartListTotalSelector,
 } from '..';
 
 describe('Cart selectors', () => {
@@ -22,6 +25,40 @@ describe('Cart selectors', () => {
         2: { size: 'medium', quantity: 4 },
       };
       expect(cartListNumSelector({ cart: { list } })).toEqual(5);
+    });
+  });
+  
+  describe('cartListIdsSelector', () => {
+    it('should return an array containing the cart list items ids', () => {
+      const list = {
+        1: { size: 'large', quantity: 1 },
+        2: { size: 'medium', quantity: 4 },
+      };
+      expect(cartListIdsSelector({ cart: { list } })).toEqual(['1', '2']);
+    });
+  });
+  
+  describe('cartIdSelector', () => {
+    it('should return the selected item props and compute the total price based on quantity', () => {
+      const list = {
+        1: { size: 'large', quantity: 1, price: '1.2' },
+        2: { size: 'medium', quantity: 4, price: '1.3' },
+      };
+      expect(cartIdSelector(2)({ cart: { list } })).toEqual({
+        size: 'medium',
+        quantity: 4,
+        price: '5.2',
+      });
+    });
+  });
+  
+  describe('cartListTotalSelector', () => {
+    it('should return the selected item props and compute the total price based on quantity', () => {
+      const list = {
+        1: { size: 'large', quantity: 5, price: '1.2' },
+        2: { size: 'medium', quantity: 4, price: '1.3' },
+      };
+      expect(cartListTotalSelector({ cart: { list } })).toEqual('11.2');
     });
   });
 });
--- client/features/cart/selectors/index.js
+++ client/features/cart/selectors/index.js
@@ -1,5 +1,6 @@
 // @flow
 import { createSelector } from 'reselect';
+import math from 'mathjs';
 import type { State } from '../../../types';
 
 export const cartListSelector = (state: State) => state.cart.list || {};
@@ -7,4 +8,37 @@ export const cartListSelector = (state: State) => state.cart.list || {};
 export const cartListNumSelector = createSelector(
   cartListSelector,
   list => Object.keys(list).reduce((acc, curr) => list[curr].quantity + acc, 0),
+);
+
+export const cartListIdsSelector = createSelector(
+  cartListSelector,
+  list => Object.keys(list).reduce((acc, curr) => [...acc, curr], []),
+);
+
+export const cartListTotalSelector = createSelector(
+  cartListSelector,
+  list => math.format(
+    Object.keys(list).reduce((acc, curr) => (
+      math.add(
+        math.multiply(
+          math.bignumber(list[curr].quantity),
+          math.bignumber(list[curr].price),
+        ),
+        acc,
+      )
+    ), math.bignumber(0)),
+  ),
+);
+
+export const cartIdSelector = (id: string) => createSelector(
+  cartListSelector,
+  list => ({
+    ...list[id],
+    price: math.format(
+      math.multiply(
+        math.bignumber(list[id].price),
+        math.bignumber(list[id].quantity),
+      ),
+    ),
+  }),
 );
